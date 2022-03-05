d7d3c9319e003723da2833cd14da32338f8e84a4
fedeclaver <fedeclaver@gmail.com>
2022-03-05 00:16:02 -0300

    clase 3 y 4

---
+++ src/components/Widget/Widget.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import cart from '../../cart.svg';
+
+function Widget(){
+return(
+   
+    <img src={cart} className="cart" alt="cart" />
+)
+
+}
+export default Widget
