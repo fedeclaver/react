30e8408d0d13d0b230fb00ff98fce11382814867
fedeclaver <fedeclaver@gmail.com>
2022-03-12 09:52:31 -0300

    add Count

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import NavBar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './Container/ItemListContainer';
+import Count from './components/ItemCount/ItemCount';
 function App() {
   return (
     <div className='App'>
     <NavBar/>
     <ItemListContainer saludo = 'Bienvenido!! '/>
+    <Count initial={0} stock={10} onAdd/>
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import './ItemCount.css';
+function ItemCount({initial, stock, onAdd}) {
+  const [cont, setcont] = useState(initial);
+
+  const Sumar=()=>{
+    if (cont < stock) {
+      setcont(cont + 1)
+    }
+  }
+  
+  const Restar=()=>{    
+    if (cont > initial) {
+      setcont(cont - 1)
+    }
+  }
+
+  return (
+
+
+
+    
+      <div className="container ">
+          <div className="countcontainer">
+        <div className="col item">
+        <button className="btn btn-info btn-sm" onClick={Sumar}>
+              +
+          </button>
+          </div>
+          <div className="col item">
+          { cont }
+          </div>
+          <div className="col item">
+          <button className="btn btn-danger btn-sm"  onClick={Restar}>
+              -
+          </button>
+          </div>
+          <div className="col item">
+        <button className="btn btn-outline-primary btn-block" onClick={()=> console.log(cont)} >       
+            Agregar al carrito
+        </button>
+
+        </div>
+      </div>
+     
+</div>
+
+
+
+  )
+}
+
+
+
+
+export default ItemCount;
