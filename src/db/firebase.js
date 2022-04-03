e3db3591283657e76ae6258ee40f9f7544dcbdd3
fedeclaver <fedeclaver@gmail.com>
2022-04-03 17:48:49 -0300

    add enviroment

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAL1OfrhTlh4L36HhXEQPWY2t4leZhGsAM",
-  authDomain: "test-290b8.firebaseapp.com",
-  projectId: "test-290b8",
-  storageBucket: "test-290b8.appspot.com",
-  messagingSenderId: "379186784219",
-  appId: "1:379186784219:web:ffcfecdec71fec964469fe"
-};
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINSEMDERID,
+  appId: process.env.APPID
+}
 
 const app = initializeApp(firebaseConfig);
 
