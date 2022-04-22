b56689274d624429da820af491fe74b47449b391
fedeclaver <fedeclaver@gmail.com>
2022-04-22 20:44:26 -0300

    add readme

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Widget from "../Widget/Widget";
 import logo from "../../logo.svg";
+import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
@@ -13,17 +14,16 @@ function NavBar() {
         </NavLink>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink to="/category/zapatillas"   style={({ isActive }) => ({
-              color: isActive ? '#fff' : '#545e6f',
-              background: isActive ? '#7600dc' : '#f0f0f0',
-            })}
-          >Zapatillas</NavLink>
-            <NavLink to="/category/remera"   style={({ isActive }) => ({
-              color: isActive ? '#fff' : '#545e6f',
-              background: isActive ? '#7600dc' : '#f0f0f0',
-            })}
-          >Remera</NavLink>           
+          <Nav className="nav me-auto">
+         
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+            <NavLink  className="nav-item" to="/category/zapatillas"   >Zapatillas</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink  className="nav-item" to="/category/remera"     >Remera</NavLink>        
+            </li> 
+            </ul>   
           </Nav>
           <Nav>           
             <NavLink  activeStyle={{ color: 'white' }} to="/cart">
