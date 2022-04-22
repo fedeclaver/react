cbf63b54c44a4f5342d5f09200541b65947a5c1b
fedeclaver <fedeclaver@gmail.com>
2022-04-22 19:00:15 -0300

    add readme

--- src/Container/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Container/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,42 +1,47 @@
 import { useState, useEffect } from "react";
-import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 import ItemDetail from "../../Components/ItemDetail/ItemDetail";
-//import { Loading } from "../../components/Loading/Loading";
+
 import { useParams } from "react-router-dom";
+import { Card, Container, Row, Spinner } from "react-bootstrap";
 
 function ItemDetailContainer() {
   const { id } = useParams();
- // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [product, setProduct] = useState([]);
   useEffect(() => {
     // obtengo datos del mock
-   
-    async function getById() {
 
+    async function getById() {
       try {
         const db = getFirestore();
-        const item = doc(db, 'items' , id);
-        const response = await getDoc(item)
-        
-        setProduct( { id: response.id, ...response.data()} );
-      //  setLoading(false);
+        const item = doc(db, "items", id);
+        const response = await getDoc(item);
+
+        setProduct({ id: response.id, ...response.data() });
+        setLoading(false);
       } catch (error) {
         /* Manejo de Errores */
       }
-      
     }
 
-    getById();    
+    getById();
   }, [id]);
 
   return (
-  
-    
-   
-      <ItemDetail product={product}   />
-    
-      
-      
+    <Container>
+      {loading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <Row className="justify-content-center">
+          <Card style={{ width: "48rem" }}>
+            <ItemDetail product={product} />
+          </Card>
+        </Row>
+      )}
+    </Container>
   );
 }
 
--- src/Container/ItemListContainer/ItemListContainer.jsx
+++ src/Container/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,12 @@
 import ItemList from "../../Components/ItemList/ItemList";
 import { useState, useEffect } from "react";
-import { Loading } from '../../Components/Loading/Loading';
 import Container from "react-bootstrap/Container";
 import { useParams } from "react-router-dom";
 import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
+import { Spinner } from "react-bootstrap";
 function ItemListContainer() {
   const [products, setProducts] = useState([]);
-  //const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const { category } = useParams();
   useEffect(() => {
     async function getAll() {
@@ -18,7 +18,7 @@ function ItemListContainer() {
         const response = await getDocs(filterQuery)
      
         setProducts(response.docs.map( prod => ({ id: prod.id, ...prod.data() }) ));
-       // setLoading(false);
+        setLoading(false);
       } catch (error) {
         
       }
@@ -37,7 +37,13 @@ function ItemListContainer() {
 
  
       <Container fluid>
+      {loading ? (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
         <ItemList products={products}/>
+        )}
       </Container>
          </div>
     
